b2f9d33c2fc1da7c23bd5d5d814e37946113c1b9
Raymw1 <rayan.rmw@gmail.com>
2021-09-28 20:24:11 -0300

    Authentication done

---
+++ src/controllers/projectController.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth.json");
+const authMiddleware = require("../middlewares/auth");
+
+router.use(authMiddleware);
+
+router.get("/", async (req, res) => {
+  return res.send("Ok");
+});
+
+module.exports = (app) => app.use("/projects", router);
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 require("./controllers/authController")(app);
+require("./controllers/projectController")(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
